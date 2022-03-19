dab0ccdc16936f7de97271c267dfd8b946a8bae6
Deepak <deepakkumar843450@gmail.com>
2022-03-19 21:14:10 +0530

    First Commit by Spring

---
+++ IOC/src/com/ioc/Bike.java
@@ -0,0 +1,10 @@
+package com.ioc;
+
+public class Bike implements Engine {
+	
+	public void start()
+	{
+		System.out.println("Bike has been started");
+	}
+
+}
---
+++ IOC/src/com/ioc/Car.java
@@ -0,0 +1,10 @@
+package com.ioc;
+
+public class Car implements Engine {
+	
+	public void start()
+	{
+		System.out.println("Car has been started");
+	}
+
+}
